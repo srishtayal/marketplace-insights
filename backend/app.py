86f800252729f16f1f49b6d29e7045bcaad5a7ac
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-27 19:13:58 +0530

    update backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-# backend/app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -37,4 +37,5 @@ def upload():
     return jsonify({"summary": summary, "pricing": pricing_suggestions, "charts": charts})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host="0.0.0.0", port=port)
